7dc158c51ab0db8325e12082578f40ed0b1efad5
Don Luc <234055589+follyofanasterisk@users.noreply.github.com>
2026-01-11 15:42:23 +0000

    Version 0.7.1
    Updates to the 2D Visualization

--- SoilFertilitySimulation/Controls/2DViewer.cs
+++ SoilFertilitySimulation/Controls/2DViewer.cs
@@ -16,7 +16,7 @@ namespace SoilFertilitySimulation.Controls
     {
         public _2DViewer()
         {
-            //AffectsRender<>
+            AffectsRender<_2DViewer>(IsRunningProperty);
         }
 
         private int[] _worldgrid = [];
@@ -43,49 +43,34 @@ namespace SoilFertilitySimulation.Controls
             AvaloniaProperty.Register<_2DViewer, bool>(
                 nameof(IsRunning));
 
-        protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
-        {
-            base.OnPropertyChanged(change);
-            if (change.Property == IsRunningProperty)
-            {
-                Dispatcher.UIThread.Post(InvalidateVisual, DispatcherPriority.Render);
-            }
-        }
-
         public override void Render(DrawingContext context)
         {
             base.Render(context);
             
             if (IsRunning)
             {
-                //transform coordinates and then get center, add the coordinates divided by distance from center to corner
                 //Make sure to catch in case screen is very tiny
-                //draw rectanges based 
-                //What scaling do you want for the screen size and grid??? Currently is 0
-                Point ScalingFactor = new Point((Bounds.Center.X - 5) / 50, (Bounds.Center.Y - 5) / 50);
+                double ScalingFactor = (Math.Min(Bounds.Center.X, Bounds.Center.Y) - 5) / 50;
                 for (int i = 0; i < WorldGrid.Length; i++)
                 {
                     double FirstTwoDigits = WorldGrid[i] % 100;
                     double LastTwoDigits = WorldGrid[i] - (WorldGrid[i] % 1000);
-                    //border problems
                     switch (WorldGrid[i] - LastTwoDigits - FirstTwoDigits)
                     {
                         case 0:
-                            context.DrawRectangle(new Pen(Brushes.White), new Rect(Bounds.Center.X + FirstTwoDigits * ScalingFactor.X,
-                                Bounds.Center.Y + (LastTwoDigits / 1000) * ScalingFactor.Y, 1, 1));
+                            context.DrawRectangle(new Pen(Brushes.Green), new Rect(Bounds.Center.X + FirstTwoDigits * ScalingFactor,
+                                Bounds.Center.Y + (LastTwoDigits / 1000) * ScalingFactor, 1, 1));
                             break;
                         case 900:
-                            context.DrawRectangle(new Pen(Brushes.White), new Rect(Bounds.Center.X + -(100 - FirstTwoDigits) * ScalingFactor.X,
-                                Bounds.Center.Y + ((LastTwoDigits / 1000) + 1) * ScalingFactor.Y, 1, 1));
+                            context.DrawRectangle(new Pen(Brushes.Green), new Rect(Bounds.Center.X + -(100 - FirstTwoDigits) * ScalingFactor,
+                                Bounds.Center.Y + ((LastTwoDigits / 1000) + 1) * ScalingFactor, 1, 1));
                             break;
                         case -900:
-                            context.DrawRectangle(new Pen(Brushes.White), new Rect(Bounds.Center.X + (100 + FirstTwoDigits) * ScalingFactor.X,
-                                Bounds.Center.Y + ((LastTwoDigits / 1000) - 1) * ScalingFactor.Y, 1, 1));
+                            context.DrawRectangle(new Pen(Brushes.Green), new Rect(Bounds.Center.X + (100 + FirstTwoDigits) * ScalingFactor,
+                                Bounds.Center.Y + ((LastTwoDigits / 1000) - 1) * ScalingFactor, 1, 1));
                             break;
                     }
                 }
-                //context.DrawRectangle(new Pen(Brushes.White), new Rect(0, 0, 10, 10));
-                //context.DrawEllipse(Brushes.White, null, new Point(100, 100), 10, 10);
                 Dispatcher.UIThread.Post(InvalidateVisual, DispatcherPriority.Render);
             }
         }
